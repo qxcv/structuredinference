be8eea3c89d8862e18ad22ad9013dc6c35359375
Sam Toyer <sam@qxcv.net>
2017-03-06 10:39:49 +1100

    IkeaDB action prediction improvements

--- expt-ikeadb/load.py
+++ expt-ikeadb/load.py
@@ -1,8 +1,6 @@
 """Load a 2D pose dataset (probably IkeaDB) and (optionally) associated
 actions."""
 
-import numpy as np
-
 from p2d_loader import load_p2d_data
 
 
@@ -15,7 +13,9 @@ def loadDataset():
         seq_skip,
         gap=1,
         val_frac=0.2,
-        add_noise=0.6)
+        completion_length=256,
+        add_noise=0.9,
+        remove_head=True)
 
     dim_observations = data["train_poses"].shape[2]
 
@@ -47,14 +47,22 @@ def loadDataset():
     dataset['train_aclass_ds'] = data["train_aclass_ds"]
     dataset['val_aclass_ds'] = data["val_aclass_ds"]
 
+    # for sequence completion
+    dataset['train_completions'] = data['train_completions']
+    dataset['val_completions'] = data['val_completions']
+
     print('Shapes of various things:')
-    to_check = [
+    to_check_shape = [
         'train', 'valid', 'test', 'train_cond_vals', 'val_cond_vals',
         'test_cond_vals'
     ]
-    for to_shape in to_check:
+    for to_shape in to_check_shape:
         print('%s: %s' % (to_shape, dataset[to_shape].shape))
-    for name in ['train_aclass_ds', 'val_aclass_ds']:
+    to_check_len = [
+        'train_aclass_ds', 'val_aclass_ds', 'train_completions',
+        'val_completions'
+    ]
+    for name in to_check_len:
         print('%s: %d (list)' % (name, len(dataset[name])))
 
     return dataset
--- expt-ikeadb/train_act_pred.py
+++ expt-ikeadb/train_act_pred.py
@@ -28,7 +28,7 @@ def make_model(seq_len, num_channels, num_actions):
     model.add(GaussianNoise(0.05, input_shape=(seq_len, num_channels)))
     model.add(
         Bidirectional(
-            GRU(96,
+            GRU(50,
                 return_sequences=True,
                 dropout_W=0.2,
                 dropout_U=0.2,
@@ -39,7 +39,7 @@ def make_model(seq_len, num_channels, num_actions):
 
     model.add(
         Bidirectional(
-            GRU(96,
+            GRU(50,
                 return_sequences=False,
                 dropout_W=0.2,
                 dropout_U=0.2,
@@ -48,10 +48,10 @@ def make_model(seq_len, num_channels, num_actions):
                 init='uniform')))
     model.add(Dropout(0.2))
 
-    model.add(Dense(96, W_regularizer=l2(0.001), activation='relu'))
+    model.add(Dense(50, W_regularizer=l2(0.001), activation='relu'))
     model.add(Dropout(0.3))
 
-    model.add(Dense(96, W_regularizer=l2(0.001)))
+    model.add(Dense(50, W_regularizer=l2(0.001)))
     model.add(Dropout(0.3))
 
     model.add(Dense(num_actions, activation='softmax'))
@@ -63,24 +63,55 @@ def make_model(seq_len, num_channels, num_actions):
     return model
 
 
-def merge_actions(act_labels, act_names, merges):
-    # TODO: all of this is broken. Need to finish it.
-    raise NotImplementedError()
+def balance_aclass_ds(aclass_ds, act_names):
+    # find appropriate number of samples for a single action class,
+    # then trim "heavy" action classes to have no more than
+    # that number of samples
+    class_map = np.zeros((len(aclass_ds), len(act_names)))
+    for ds_idx, item in enumerate(aclass_ds):
+        _, class_num = item
+        class_map[ds_idx, class_num] = 1
+    support = class_map.sum(axis=0)
+    support_target = int(np.min(support))
+    to_keep = np.zeros((len(aclass_ds), ))
+    for class_num in range(len(act_names)):
+        if support[class_num] <= support_target:
+            to_keep[class_map[:, class_num] == 1] = 1
+        else:
+            # drop all but [:median_support] of these
+            class_inds, = np.nonzero(class_map[:, class_num])
+            perm = np.random.permutation(len(class_inds))[:support_target]
+            chosen_inds = class_inds[perm]
+            to_keep[chosen_inds] = 1
+    rv = []
+    for choose_ind in np.nonzero(to_keep)[0]:
+        rv.append(aclass_ds[choose_ind])
+    return rv
 
-    act_labels = np.asarray(act_labels)
-    new_labels = np.empty_like(act_labels)
 
-    # start by making new labels array this will help us figure out which
-    # indices should map to which
-    inverse_merges = {}
-    for target, sources in merges.items():
-        for source in sources:
-            assert source not in inverse_merges
-            inverse_merges[source] = target
-
-    # ind_map should be a len(act_names) array of integers representing labels
-    # under the newly constructed labelling system
-    ind_map = []
+def merge_actions(aclass_ds, merge_map, act_names):
+    for class_name in act_names:
+        if class_name not in merge_map:
+            merge_map[class_name] = class_name
+    new_class_names = sorted({
+        class_name
+        for class_name in merge_map.values() if class_name is not None
+    })
+    new_class_nums = []
+    for class_name in act_names:
+        new_name = merge_map[class_name]
+        if new_name is None:
+            new_num = None
+        else:
+            new_num = new_class_names.index(new_name)
+        new_class_nums.append(new_num)
+    new_aclass_ds = []
+    for poses, action in aclass_ds:
+        new_action = new_class_nums[action]
+        if new_action is None:
+            continue
+        new_aclass_ds.append((poses, new_action))
+    return new_class_names, new_aclass_ds
 
 
 def to_XY(ds):
@@ -89,9 +120,8 @@ def to_XY(ds):
     good_samples = Y_ints != 0
     Y = to_categorical(Y_ints, num_classes)
 
-    # TODO: try the following:
-    # 1) Reconstruct poses first, then subtract out the mean pose
-    # 2) Extend pose setup by concatenating with previous frame (done)
+    # TODO: try to reconstruct poses before learning on them. Maybe subtract
+    # out mean pose of each sequence.
     T, D = ds[0][0].shape
     X = np.empty((len(ds), T, D))
     for idx, pair in enumerate(ds):
@@ -106,13 +136,56 @@ def to_XY(ds):
     return X_cat[good_samples], Y[good_samples]
 
 
+def standardise(features, mean=None, std=None):
+    assert features.ndim == 3, \
+        "expected features to be 3D (N*T*D), but shape was %s" \
+        % (features.shape,)
+
+    if mean is None:
+        mean = features.reshape((-1, features.shape[-1])) \
+                       .mean(axis=0)
+
+    if std is None:
+        std = features.reshape((-1, features.shape[-1])) \
+                       .std(axis=0)
+
+    std[std < 1e-5] = 1
+    shape_mean = mean[np.newaxis, np.newaxis, ...]
+    shape_std = std[np.newaxis, np.newaxis, ...]
+    features = (features - shape_mean) / shape_std
+
+    return features, mean, std
+
+
 if __name__ == '__main__':
     dataset = loadDataset()
-    train_X, train_Y = to_XY(dataset['train_aclass_ds'])
-    val_X, val_Y = to_XY(dataset['val_aclass_ds'])
+    merge_map = {
+        'attach leg 1': 'attach leg',
+        'attach leg 2': 'attach leg',
+        'attach leg 3': 'attach leg',
+        'attach leg 4': 'attach leg',
+        'detach leg 1': 'detach leg',
+        'detach leg 2': 'detach leg',
+        'detach leg 3': 'detach leg',
+        'detach leg 4': 'detach leg',
+        'n/a': None
+    }
+    old_act_names = dataset['p2d_action_names']
+    _, train_aclass_ds \
+        = merge_actions(dataset['train_aclass_ds'], merge_map, old_act_names)
+    aclass_target_names, val_aclass_ds \
+        = merge_actions(dataset['val_aclass_ds'], merge_map, old_act_names)
+    train_aclass_ds_bal = balance_aclass_ds(train_aclass_ds,
+                                            aclass_target_names)
+    val_aclass_ds_bal = balance_aclass_ds(val_aclass_ds, aclass_target_names)
+    train_X, train_Y = to_XY(train_aclass_ds_bal)
+    val_X, val_Y = to_XY(val_aclass_ds_bal)
 
-    assert train_Y[:, 0].sum() == 0, "should have nothing of class 0"
-    assert val_Y[:, 0].sum() == 0, "should have nothing of class 0"
+    train_X, mean, std = standardise(train_X)
+    val_X, _, _ = standardise(val_X, mean, std)
+
+    # assert train_Y[:, 0].sum() == 0, "should have nothing of class 0"
+    # assert val_Y[:, 0].sum() == 0, "should have nothing of class 0"
 
     checkpoint_dir = './chkpt-aclass/'
     try:
