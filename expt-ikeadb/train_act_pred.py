b75bb9768be2a1c4a670c71445d39fba1b872452
Sam Toyer <sam@qxcv.net>
2017-03-13 16:44:53 +1100

    Use different classes, balance properly

--- expt-ikeadb/train_act_pred.py
+++ expt-ikeadb/train_act_pred.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Trains an action predictor on estimated IkeaDB poses."""
 
+import numpy as np
+
 import addpaths  # noqa
 from load import loadDataset
 from common_pp.act_class_model import train_act_class_model
@@ -9,14 +11,16 @@ from common_pp.act_class_model import train_act_class_model
 if __name__ == '__main__':
     dataset = loadDataset()
     merge_map = {
-        'attach leg 1': 'attach leg',
-        'attach leg 2': 'attach leg',
-        'attach leg 3': 'attach leg',
-        'attach leg 4': 'attach leg',
-        'detach leg 1': 'detach leg',
-        'detach leg 2': 'detach leg',
-        'detach leg 3': 'detach leg',
-        'detach leg 4': 'detach leg',
+        'attach leg 1': '*tach',
+        'attach leg 2': '*tach',
+        'attach leg 3': '*tach',
+        'attach leg 4': '*tach',
+        'detach leg 1': '*tach',
+        'detach leg 2': '*tach',
+        'detach leg 3': '*tach',
+        'detach leg 4': '*tach',
+        'spin in': 'spin',
+        'spin out': 'spin',
         'n/a': None
     }
-    train_act_class_model(dataset, merge_map)
+    train_act_class_model(dataset, merge_map, balance_func=np.median)
